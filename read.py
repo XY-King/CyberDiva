14ed560bfa2666c7aa5e768c050ce836c34e8bd0
ausummer <ausummer.sjtu.edu.cn>
2023-08-21 17:01:24 +0800

    chore: make use of openai embedding util

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 import json
 import string
 import openai
+from openai.embeddings_utils import get_embeddings
 
 def get_chara_config(api_key: string):
     name = json.load(open("chara.json", "rb"))["name"]
@@ -16,17 +17,12 @@ def get_chara_config(api_key: string):
 def embed_chara(name: string, api_key: string):
     charaInit = json.load(open(f"characters/{name}.json", "rb"))
     openai.api_key = api_key
-    saying_embeddings = openai.Embedding.create(
-                        model="text-embedding-ada-002",
-                        input=charaInit["sayings"]
-                    )
-    saying_embeddings = saying_embeddings["data"]
-    story_embeddings = openai.Embedding.create(
-                        model="text-embedding-ada-002",
-                        input=charaInit["story"]
-                    )
-    story_embeddings = story_embeddings["data"]
+
+    # get the embeddings for the sayings and the story
+    saying_embeddings = get_embeddings(list_of_text=charaInit["sayings"], engine="text-embedding-ada-002")
+    story_embeddings = get_embeddings(list_of_text=charaInit["story"], engine="text-embedding-ada-002")
     
+    # make the json file of the character with the embeddings
     sayings_embedded = []
     for i in range(len(charaInit["sayings"])):
         embedding = saying_embeddings[i]["embedding"]
@@ -37,9 +33,11 @@ def embed_chara(name: string, api_key: string):
         embedding = story_embeddings[i]["embedding"]
         story_embedded.append({"content": charaInit["story"][i], "embedding": embedding})
 
+    # change the "is_embedded" to True
     charaInit["is_embedded"] = True
     with open(f"characters/{name}.json", "w", encoding="UTF-8") as f:
         json.dump(charaInit, f, ensure_ascii=False, indent=4)
+    # output the json file with embeddings
     charaInit["sayings"] = sayings_embedded
     charaInit["story"] = story_embedded
     with open(f"characters/{name}_embedded.json", "w", encoding="UTF-8") as f:
