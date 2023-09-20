cc382253cbc8562673b0195f1b2333cb87fff074
ausummer <ausummer.sjtu.edu.cn>
2023-09-20 18:17:38 +0800

    feat: main qt interface done

--- script.py
+++ script.py
@@ -3,6 +3,12 @@ from charaChat import CharaChat
 from read import get_chara_config, get_user_config, get_api_key
 import os
 from flask import Flask, request, render_template
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QHBoxLayout   
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl, Qt
+from PyQt5.QtGui import QCursor
+import threading
+
 
 app = Flask(__name__)
 get_api_key()
@@ -34,6 +40,66 @@ def chat():
         print(reaction)
     return {"action_list": action_list}
 
+def run_app():
+    app.run(host="localhost", port=5000, debug=False)
+
+class MainWindow(QMainWindow):
+    m_drag_position = None
+
+    def __init__(self):
+        super().__init__()
+
+        # The circle widget
+        self.button = QPushButton('')
+        self.button.setStyleSheet("""
+            QPushButton {
+                background-color: purple;
+                border: none;
+                border-radius: 20px;
+                min-width: 40px;
+                min-height: 40px;
+            }
+        """)
+        self.button.pressed.connect(self.start_drag)
+
+        # The web view
+        self.web = QWebEngineView()
+        self.web.load(QUrl("http://localhost:5000"))
+        self.web.page().setBackgroundColor(Qt.transparent)
+        self.web.setFixedSize(320, 720)
+
+        # Create a layout and add the widgets
+        layout = QHBoxLayout()
+        layout.addWidget(self.button)
+        layout.addWidget(self.web)
+
+        # Create a QWidget and set the layout
+        centralWidget = QWidget()
+        centralWidget.setLayout(layout)
+
+        # Set the central widget
+        self.setCentralWidget(centralWidget)
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_TranslucentBackground, True)
+
+    def start_drag(self):
+        MainWindow.m_drag_position = self.mapFromGlobal(QCursor.pos())
+        QApplication.setOverrideCursor(Qt.ClosedHandCursor)
+
+    def mouseMoveEvent(self, event):
+        if MainWindow.m_drag_position:
+            self.move(self.mapToGlobal(event.pos()) - MainWindow.m_drag_position)
+
+    def mouseReleaseEvent(self, event):
+        QApplication.restoreOverrideCursor()
+        MainWindow.m_drag_position = None
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
+    t = threading.Thread(target=run_app)
+    t.daemon = True
+    t.start()
+
+    appQt = QApplication([])
+    window = MainWindow()
+    window.show()
+    appQt.exec_()
